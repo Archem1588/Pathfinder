7b424fee22cbc26ec1631c78d1f8782b395cbf1e
Hannah Park <hannahpark@dhcp-128-189-214-125.ubcsecure.wireless.ubc.ca>
2015-10-26 14:03:18 -0700

    Database setup

---
+++ bikeways/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from xml.etree.ElementTree import ElementTree
+
+# Create your models here.
+
+class CoordinateData(models.Model):
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+    #
+    # @classmethod
+    # def create(cls, lat, lng):
+    #     c = cls(latitude=lat, longitude=lng)
+    #     return c
+
+    # Return latitude for now
+    def __str__(self):
+        return self.lat
+
