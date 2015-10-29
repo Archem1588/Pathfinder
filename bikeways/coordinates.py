4e3145b4d03b9d1367252255787bc01be7f7cb82
Hannah Park <hannahpark@Hannahs-MacBook-Pro.local>
2015-10-28 23:07:32 -0700

    Data->KML and fetching points from DB

---
+++ bikeways/coordinates.py
@@ -0,0 +1,19 @@
+__author__ = 'hannahpark'
+
+import sqlite3
+import os
+import json
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+db = os.path.join(BASE_DIR,'db.sqlite3')
+
+def getPoint():
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    c.execute('SELECT * FROM bikeways_coordinatedata')
+    rows = c.fetchall()
+    for row in rows:
+        print(row[1]) # latitudes from database
+    c.close()
+    conn.close()
+
+getPoint()
