bfdf44e35e85935700caac957629715c1b27f96c
Hannah Park <hannahpark@Hannahs-MacBook-Pro.local>
2015-11-02 17:34:53 -0800

    Need to commit for Heroku to work

--- bikeways/coordinates.py
+++ bikeways/coordinates.py
@@ -1,12 +1,15 @@
 import sqlite3
 import os
 import json
+import psycopg2
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 db = os.path.join(BASE_DIR,'db.sqlite3')
 
 
 def connectDB():
-    conn = sqlite3.connect(db)
+    #conn = sqlite3.connect(db)
+    conn_string = "host='localhost' dbname='tomato' user='postgres' password='postgres'"
+    conn = psycopg2.connect(conn_string)
     c = conn.cursor()
     c.execute('SELECT * FROM bikeways_coordinatedata')
     rows = c.fetchall()
@@ -18,7 +21,7 @@ def getLats():
     rows = connectDB()
     lats = []
     for row in rows:
-        lats.append(row[2])
+        lats.append(row[1])
     lat = lats[0] # just getting the first lat point
     return json.dumps(lat)
 
@@ -26,7 +29,7 @@ def getLngs():
     rows = connectDB()
     lngs = []
     for row in rows:
-        lngs.append(row[1])
+        lngs.append(row[2])
     lng = lngs[0] # just getting the first lng point
     return json.dumps(lngs)
 
@@ -34,7 +37,7 @@ def getCoords():
     rows = connectDB()
     coordinates = []
     for row in rows:
-        coordinates.append({'lat': row[2], 'lng': row[1]})
+        coordinates.append({'lat': row[1], 'lng': row[2]})
     return(json.dumps(coordinates))
 
 getCoords()
