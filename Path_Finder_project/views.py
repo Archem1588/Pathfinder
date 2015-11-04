cc2486ae98c7496de031d19d9caf20ee28b04c41
Hannah Park <hannahpark@dhcp-128-189-210-223.ubcsecure.wireless.ubc.ca>
2015-11-03 16:39:52 -0800

    4th try

--- Path_Finder_project/views.py
+++ Path_Finder_project/views.py
@@ -1,24 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from bikeways.coordinates import getLats, getLngs, getCoords
+#from bikeways.coordinates import getCoords
 from django.contrib.admin.views.decorators import staff_member_required
 
 
 def home(request):
-    #getPoints()
-    #if request.is_ajax() or request.method == 'GET':
-        #return render(request, 'Success')
     return render(request, 'bikeways/home.html')
-    #lat = getLngs()
-    #return HttpResponse(lat, content_type='application/json')
 
-# def admin(request):
-#     return render(request, 'templates/bikeways/admin.html')
-
-#def getPoints():
-#    lat = getLngs()
-#    return HttpResponse(lat, content_type='application/json')
-
-def coord_test(request):
-    coord = getCoords()
-    return HttpResponse(coord, content_type='application/json')
+#def coord_test(request):
+    #coord = getCoords()
+    #return HttpResponse(coord, content_type='application/json')
