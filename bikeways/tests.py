643346adc5852d8201e3eedf5f488cd9c0ce4d9f
dmatis <darren.matis@gmail.com>
2015-11-05 14:16:56 -0800

    Now detects if admin or visitor on home, and displays log in/log out

--- bikeways/tests.py
+++ bikeways/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+import unittest
 
-# Create your tests here.
+
+class TestBasic(unittest.TestCase):
+    "Upload Data tests"
+
+
+    def test_fails_upload(self):
+        a = 1
+        self.assertEquals(1,a)
+
+
+    def test_successful_upload(self):
+        a = 1
+        assert a == 1
+
