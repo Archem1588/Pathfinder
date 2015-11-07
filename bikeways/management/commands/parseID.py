1f90ad7c351d08efcb6c706e97245ccfa737171e
Hannah Park <hannahpark@Hannahs-MacBook-Pro.local>
2015-11-07 08:53:08 -0800

    script for parsing kml with key associated with each coordinate

---
+++ bikeways/management/commands/parseID.py
@@ -0,0 +1,40 @@
+__author__ = 'hannahpark'
+from django.core.management.base import BaseCommand
+from xml.etree.ElementTree import ElementTree
+from bikeways.models import CoordinateWithID
+import os
+import django
+
+
+class Command(BaseCommand):
+    args = '<foo bar ...>'
+    help = 'our help string comes here'
+
+    def _create_tags(self):
+
+        tree = ElementTree()
+        data = tree.parse('bikeways.kml')
+        placemark = data.findall('.//{http://www.opengis.net/kml/2.2}Placemark')
+
+        i = 1
+        for multiGeom in placemark:
+            placemarkid = i
+            for item in multiGeom:
+                for lineString in item:
+                    for coord in lineString:
+                        key = placemarkid
+                        lng = coord.text[0:12]
+                        lat = coord.text[18:30]
+                        # print(key)
+                        # print(lat)
+                        # print(lng)
+                        c = CoordinateWithID(key=key, latitude=float(lat), longitude=float(lng))
+                        c.save()
+
+            i += 1
+
+
+    def handle(self, *args, **options):
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'epl.settings')
+        django.setup()
+        self._create_tags()
