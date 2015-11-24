67ea552c7ddb222a1f76783ddb716fd7afd59a04
David Park <davidpark.1993@gmail.com>
2015-11-24 09:32:37 +0900

    Test

--- bikeways/management/commands/parseID.py
+++ bikeways/management/commands/parseID.py
@@ -1,4 +1,3 @@
-__author__ = 'hannahpark'
 from django.core.management.base import BaseCommand
 from xml.etree.ElementTree import ElementTree
 from bikeways.models import CoordinateWithID
@@ -22,34 +21,18 @@ class Command(BaseCommand):
             placemarkid = i
             for item in multiGeom:
                 for lineString in item:
-                    for coord in lineString: #for a single line in bikeways.kml
+                    for coord in lineString:
                         key = placemarkid
                         splitcoords = coord.text
-                        splitbycomma = re.split(',0 |,',splitcoords) #[49,123,'']
+                        splitbycomma = re.split(',0 |,', splitcoords)
                         for index in range(len(splitbycomma)-1):
                             if index % 2 == 0:
                                 lng = splitbycomma[index]
                                 lat = splitbycomma[index+1]
                                 c = CoordinateWithID(key=key, latitude=float(lat), longitude=float(lng))
-                                print(key)
-                                print(lat)
-                                print(lng)
-                                # c.save()
-
-                        # lng = coord.text[0:12]
-                        # lat = coord.text[18:30]
-                        #
-                        # latString = str(lat)
-                        # latFirstDigit = latString[0]
-                        # if latFirstDigit != "4":
-                        #     lat = coord.text[17:29]
-                        # latString = str(lat)
-                        # latFirstDigit = latString[0]
-                        # if latFirstDigit != "4":
-                        #     lat = coord.text[16:28]
+                                c.save()
 
             i += 1
-
 
     def handle(self, *args, **options):
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'epl.settings')
